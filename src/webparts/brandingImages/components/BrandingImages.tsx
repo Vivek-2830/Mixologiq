53a1e23987ff575888e61f1b9553a3b49760022c
Vivek Gohil <gohilvivek0303@gmail.com>
2025-09-01 18:34:41 +0530

    Complete Branding Images webpart

--- src/webparts/brandingImages/components/BrandingImages.tsx
+++ src/webparts/brandingImages/components/BrandingImages.tsx
@@ -31,6 +31,45 @@ export default class BrandingImages extends React.Component<IBrandingImagesProps
     return (
       <section id="brandingImages">
         
+        <div className='BrandingImages-Section'>
+          <h2>Branding Image</h2>
+          <p>Marketing materials and branded visual assets</p>
+        </div>
+
+        <div className='ms-Grid'>
+          <div className='ms-Grid-row'>
+            <div className='ms-Grid-col ms-sm12 ms-md12 ms-lg12'>
+
+              <div className='Image-Section'>
+                <div className='ms-Grid-col ms-sm6 ms-md3 ms-lg3'>
+                  <div className='Image-Container'>
+                    <img src={require("../assets/Images/barenz-14.jpg")} alt="Branding Cocktail" />
+                  </div>
+                </div>
+
+                <div className='ms-Grid-col ms-sm6 ms-md3 ms-lg3'>
+                  <div className='Image-Container'>
+                    <img src={require("../assets/Images/man-preparing-cocktails.jpg")} alt="Branding Cocktail" />
+                  </div>
+                </div>
+
+                <div className='ms-Grid-col ms-sm6 ms-md3 ms-lg3'>
+                  <div className='Image-Container'>
+                    <img src={require("../assets/Images/OIP.jpg")} alt="Branding Cocktail" />
+                  </div>
+                </div>
+
+                <div className='ms-Grid-col ms-sm6 ms-md3 ms-lg3'>
+                  <div className='Image-Container'>
+                    <img src={require("../assets/Images/Ruby_Claire_Geneva_web_Bar_Nighttime-4.jpg")} alt="Branding Cocktail" />
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+        
       </section>
     );
   }
